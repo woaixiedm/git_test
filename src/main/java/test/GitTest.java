36b15e09f4e7d5655e7793666f6b864ffd117a25
yzw <yzw@qq.com>
2021-11-12 14:19:10 +0800

    thrid change

--- src/main/java/test/GitTest.java
+++ src/main/java/test/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,gitTest");
         System.out.println("second,change");
+        System.out.println("thrid,change");
     }
 
 }
